f4d11086f3468d98d5194cf01753c653052f87cf
Mendoza210727 <mendoza.mc27@gemail.com>
2026-02-07 17:28:30 -0400

    cuarto commit

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,5 @@
-FROM maven:3-jdk-8-alpine as builder
-
-WORKDIR /usr/src/app
-
-COPY . /usr/src/app
-RUN mvn package
-
-FROM openjdk:8-jre-alpine
-
-COPY --from=builder /usr/src/app/target/*.jar /app.jar
-
-EXPOSE 8080
-
-ENTRYPOINT ["java"]
-CMD ["-jar", "/app.jar"]
+FROM openjdk:11-jre-slim
+VOLUME /tmp
+ARG JAR_FILE=target/demo-0.0.1-SNAPSHOT.jar
+COPY ${JAR_FILE} app.jar
+ENTRYPOINT ["java","-jar","/app.jar"]
--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,21 +1,21 @@
 package com.example.demo;
 
-import org.springframework.boot.*;
-import org.springframework.boot.autoconfigure.*;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
 
-
-//test
-	@GetMapping("/")
-	public String home() {
-		return "Spring is here!";
-	}
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+@RestController
+class HelloController {
+    @GetMapping("/")
+    public String hello() {
+        return "Hello CI/CD World!";
+    }
 }
